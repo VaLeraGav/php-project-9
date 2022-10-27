0f873c9dd0e67e3e9f5634f73db45761a446d53f
Valeriy <95502737+VaLeraGav@users.noreply.github.com>
2022-10-27 22:45:55 +0300

    add UrlCheckController and table

---
+++ app/Http/Controllers/UrlCheckController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UrlCheckController extends Controller
+{
+    public function store(Request $request)
+    {
+        // return view('urls.index');
+    }
+}
