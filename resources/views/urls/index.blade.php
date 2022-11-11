6829e37933eaad883f61fe3ee1e288cca344d622
Valeriy <95502737+VaLeraGav@users.noreply.github.com>
2022-11-11 14:40:34 +0300

    hexlet-check: remove '<thead>'

--- resources/views/urls/index.blade.php
+++ resources/views/urls/index.blade.php
@@ -7,15 +7,13 @@
         <h1>Сайты</h1>
         <div class="table-responsive">
             <table class="table table-bordered table-hover text-nowrap">
-                <thead>
+                <tbody>
                 <tr>
                     <th>ID</th>
                     <th>Имя</th>
                     <th>Последняя проверка</th>
                     <th>Код ответа</th>
                 </tr>
-                </thead>
-                <tbody>
                 @foreach($urls as $url)
                     <tr>
                         <td>{{ $url->id }}</td>
