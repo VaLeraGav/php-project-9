4c5869ef02f07aeffed265122d3c37d99e7676aa
Valeriy <95502737+VaLeraGav@users.noreply.github.com>
2022-11-17 17:50:27 +0300

    fix mix

---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+const mix = require('laravel-mix');
+
+// mix.styles([
+//     'resources/css/app.css',
+// ], 'public/css/app.css');
+//
+// mix.scripts([
+//     'resources/js/app.js',
+// ], 'public/js/app.js');
