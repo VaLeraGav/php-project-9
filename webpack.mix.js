e60c14f869d6651595af5cd5954e47e743763af1
Valeriy <95502737+VaLeraGav@users.noreply.github.com>
2022-11-17 16:58:47 +0300

    rework styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,16 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+// mix.js('resources/js/app.js', 'public/js')
+//     .postCss('resources/css/app.css', 'public/css', [
+//         //
+//     ]);
+
+//
+// mix.styles([
+//     'resources/css/app.css',
+// ], 'public/css/app.css');
+//
+// mix.scripts([
+//     'resources/js/app.js',
+// ], 'public/js/app.js');
